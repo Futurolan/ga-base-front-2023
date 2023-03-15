e7f5385a4694220454a4d17ffc10a02fbf07924b
Bmagic <contact@bmagic.fr>
2023-03-15 09:14:01 +0100

    Fix missign url & image

--- components/family/FamilyActivity/index.js
+++ components/family/FamilyActivity/index.js
@@ -6,20 +6,20 @@ import './styles.scss'
 const FamilyActivity = (props) => (
   <div className='box family-card'>
     <div className='columns'>
-      <div className='column is-3 is-3-tablet'>
+      {(props.imageUrl || props.url) && <div className='column is-3 is-3-tablet'>
         <div className='has-text-centered'>
           <img src={props.imageUrl} />
           {props.url && <a className='button is-primary is-fullwidth' target='_blank' href={props.url}>Site web</a>}
         </div>
-      </div>
-      <div className='column is-9'>
+      </div>}
+      <div className={`column ${props.imageUrl || props.url ? 'is-9' : 'is-12'}`} >
         <div className='description'>
           <h2 className='title is-5 is-marginless'>{props.title}</h2>
           {props.description && <div className='content has-text-justified' dangerouslySetInnerHTML={{ __html: props.description }} />}
         </div>
       </div>
     </div>
-  </div>
+  </div >
 )
 
 FamilyActivity.propTypes = {
